ed8049e5c9967023fc06be75b91ee1601ffcd658
JlFerrete <joseluisferrete@gmail.com>
2019-10-13 08:45:35 +0200

    Currency pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,26 @@ export class AppComponent {
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI = Math.PI;
   a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("LLego la data!"), 3500);
+  });
+
+  fecha = new Date();
+
+  video = "mOeSfOJrUIk";
+
+  activar: boolean = true;
 }
